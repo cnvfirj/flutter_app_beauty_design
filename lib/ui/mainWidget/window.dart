660d8af6c1f55d94eb267f9e1f19372881b0b856
Admin <cnvfirj@gmail.com>
2021-10-14 18:06:15 +0300

    back text

--- lib/ui/mainWidget/window.dart
+++ lib/ui/mainWidget/window.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_beauty_design/generated/l10n.dart';
 
 import 'package:url_launcher/url_launcher.dart';
-
+import 'dart:ui' as grad;
 
 /*подробнее о кастомных апп барах
 * https://stackoverflow.com/questions/52678469/the-appbardesign-cant-be-assigned-to-the-parameter-type-preferredsizewidget*/
@@ -60,7 +60,16 @@ class BackgroundMainText extends StatelessWidget{
       child:Text(
         S.maybeOf(context)!.background_text,
         style: TextStyle(
-        fontSize: 30
+            fontSize: 30,
+            foreground: Paint()
+              ..shader = grad.Gradient.linear(
+                const Offset(0, 20),
+                const Offset(0, 500),
+                [
+                  Colors.black38,
+                  Colors.black12,
+                ],
+              )
         ),
         textAlign: TextAlign.center,
     )
