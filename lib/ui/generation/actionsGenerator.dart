92cdaccc0e68c2a2f87b628f424317eba45b6b77
cnvfirj <cnvfirj@gmail.com>
2021-10-23 11:12:03 +0300

    action generate

--- lib/ui/generation/actionsGenerator.dart
+++ lib/ui/generation/actionsGenerator.dart
@@ -1,6 +1,7 @@
 
 
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter_app_beauty_design/help/byCode.dart';
 
 typedef SelectorState = Function(PresentIdentificator identificator);
@@ -121,5 +122,107 @@ class FormGenerate{
     return this;
   }
 
+}
+
+class _GenerateBigNumber extends _SourceGenerate{
+
+  late int _delta;
+  late int _start;
+  late int _end;
+
+  @override
+  void _prepared() {
+    if(_from<_to){
+      _delta = (_to-_from).abs()+1;
+      _start = _from;
+      _end = _to;
+    }
+    else {
+      _delta = (_from-_to).abs()+1;
+      _start = _to;
+      _end = _from;
+    }
+  }
+
+  @override
+  int _value() {
+    _prepared();
+    Random r = Random();
+    int value = r.nextInt(_delta);
+    return _compare(value+_start);
+  }
+
+  int _compare(int value){
+    if(_excludes.contains(value)) return _compare(value+1);
+    if(value>_end) return _compare(_start);
+    return value;
+  }
+
+}
+
+class _GenerateCommonNumber extends _SourceGenerate{
+
+  late List<int>_arr;
+
+  @override
+  void _prepared() {
+    int delta = (_to-_from).abs()+1;
+    int start = _to;
+    if(_to<_from){
+      delta = (_from-_to).abs()+1;
+      start = _from;
+    }
+    _arr = List.generate(delta-_excludes.length, (index) => _compare(start, index));
+  }
+
+  @override
+  int _value() {
+    _prepared();
+    Random r = Random();
+    int index = r.nextInt(_arr.length);
+    return _arr[index];
+  }
+
+  int _compare(int start,int index){
+    int value = start+index;
+    if(_excludes.contains(value)) return _compare(start+1, index);
+    return value;
+  }
+
+}
+
+
+typedef GetNumber = Function(int number);
+
+ abstract class _SourceGenerate{
+
+ late int _from;
+ late int _to;
+
+ _SourceGenerate _setBoundaries({required int from, required int to}){
+   _from = from;
+   _to = to;
+   return this;
+ }
+
+ late List<int>_excludes;
+
+ _SourceGenerate _setExcludes(List<int>list){
+
+   _excludes = list;
+   return this;
+ }
+
+  Future<int> _future(){
+   return Future(() => _value());
+  }
+
+  void _generate(GetNumber number) {
+   _future().then((value) => number);
+  }
+
+  void _prepared();
+
+  int _value();
 }
 
