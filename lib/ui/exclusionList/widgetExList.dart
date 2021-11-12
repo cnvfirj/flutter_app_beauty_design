1c48004bd3fb7c615aaa4645ef9a4e60fa109a3f
Admin <cnvfirj@gmail.com>
2021-11-12 21:39:17 +0200

    яя

--- lib/ui/exclusionList/widgetExList.dart
+++ lib/ui/exclusionList/widgetExList.dart
@@ -104,7 +104,6 @@ class WidgetListEx extends StatelessWidget {
                                    ),
                                  ))),
                        ]))
-
            ]);
     });
   }
