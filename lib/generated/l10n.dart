b186f9fa4eea31837b77ea73e3d86e632db3ea9f
cnvfirj <cnvfirj@gmail.com>
2021-12-02 09:39:00 +0200

    release

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -267,7 +267,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
-      Locale.fromSubtags(languageCode: 'ua'),
+      Locale.fromSubtags(languageCode: 'ru'),
+      Locale.fromSubtags(languageCode: 'uk'),
     ];
   }
 
